7382ff9609bc530801fc6ae40eb50c441a9044ea
Vyacheslav Maratkanov <slava_maratkanov@mail.ru>
2020-11-24 16:12:19 +0500

    load DictionaryGenerator

--- DictionaryGenerator/src/main/java/DictionaryGenerator.java
+++ DictionaryGenerator/src/main/java/DictionaryGenerator.java
@@ -1,34 +1,27 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DictionaryGenerator {//Я использовал детерминированные методы и так как это функции они возвращают значения
 
-    private String offer;
-
-    public DictionaryGenerator() {
-    }
-
     public static List<String> getWordList(String offer) {
-        List<String> wordList = new ArrayList<>();
-
         String[] split = offer.split(" ");
-
-        for (int i = 0; i < split.length; i++) {
-            wordList.add(split[i] + "\n");
-        }
-
-        return wordList;
+        Stream<String> splitList = Arrays.stream(split);
+        return splitList
+                .collect(Collectors.toList());
     }
 
     public static List<String> sorter(List<String> notSorted) {
-        List<String> sortedDictionary = notSorted;
-        sortedDictionary.sort(String::compareTo);
-        return sortedDictionary;
+        Stream<String> sortedStream = notSorted.stream();
+        return sortedStream
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
     }
 
     public String getString(List<String> list) {
         StringBuilder builder = new StringBuilder();
-        list.forEach(x -> builder.append(x));
+        list.forEach(x -> builder.append(x + "\n"));
         return builder.toString();
     }
 }
