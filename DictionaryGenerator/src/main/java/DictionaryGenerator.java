55b92a092d1b2fb97b1c40cc4593aefc32e96728
Vyacheslav Maratkanov <slava_maratkanov@mail.ru>
2020-12-01 15:44:45 +0500

    Patch 5

--- DictionaryGenerator/src/main/java/DictionaryGenerator.java
+++ DictionaryGenerator/src/main/java/DictionaryGenerator.java
@@ -1,17 +1,14 @@
 import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DictionaryGenerator {//Я использовал детерминированные методы и так как это функции они возвращают значения
 
     public static String getWordList(String offer) {
-        Stream<String> toSet = Arrays.stream(offer.split(" "));
-        Set<String> wordList = toSet
-                .collect(Collectors.toSet());
-
         StringBuilder builder = new StringBuilder();
-        wordList.forEach(x -> builder.append(x + "\n"));
+        Stream<String> toSet = Arrays.stream(offer.split(" "));
+        toSet
+                .filter(x -> !(builder.toString().contains(x)))
+                .forEach(x -> builder.append(x + "\n"));
         return builder.toString();
     }
 }
